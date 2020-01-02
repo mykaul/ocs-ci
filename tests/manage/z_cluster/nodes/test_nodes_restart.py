f559e5cb16dd13e5242ea43c88457c0f64a3fad0
Elad Ben Aharon <ebenahar@redhat.com>
2020-01-02 16:42:14 +0200

    More nodes restart tests
    Signed-off-by: Elad Ben Aharon <ebenahar@redhat.com>

--- tests/manage/z_cluster/nodes/test_nodes_restart.py
+++ tests/manage/z_cluster/nodes/test_nodes_restart.py
@@ -51,23 +51,38 @@ class TestNodesRestart(ManageTest):
     def test_nodes_restart(self, nodes, pvc_factory, pod_factory, force):
         """
         Test nodes restart (from the platform layer, i.e, EC2 instances, VMWare VMs)
+
         """
         ocp_nodes = get_node_objs()
         nodes.restart_nodes(nodes=ocp_nodes, force=force)
         self.sanity_helpers.health_check()
         self.sanity_helpers.create_resources(pvc_factory, pod_factory)
 
+    @pytest.mark.polarion_id("OCS-2015")
+    def test_roll_nodes_restart(self, nodes, pvc_factory, pod_factory):
+        """
+        Test restart nodes one after the other and check health status in between
+
+        """
+        ocp_nodes = get_node_objs()
+        for node in ocp_nodes:
+            nodes.restart_nodes(nodes=[node])
+            self.sanity_helpers.health_check()
+        self.sanity_helpers.create_resources(pvc_factory, pod_factory)
+
     @pytest.mark.parametrize(
-        argnames=["interface", "operation"],
+        argnames=["resource", "operation"],
         argvalues=[
-            pytest.param(*['rbd', 'create_resources'], marks=[pytest.mark.polarion_id("OCS-1138")]),
-            pytest.param(*['rbd', 'delete_resources'], marks=[pytest.mark.polarion_id("OCS-1241")]),
-            pytest.param(*['cephfs', 'create_resources'], marks=[pytest.mark.polarion_id("OCS-1139")]),
-            pytest.param(*['cephfs', 'delete_resources'], marks=[pytest.mark.polarion_id("OCS-1242")])
+            pytest.param(*['rbd-provisioner', 'create_resources'], marks=[pytest.mark.polarion_id("OCS-1138")]),
+            pytest.param(*['rbd-provisioner', 'delete_resources'], marks=[pytest.mark.polarion_id("OCS-1241")]),
+            pytest.param(*['cephfs-provisioner', 'create_resources'], marks=[pytest.mark.polarion_id("OCS-1139")]),
+            pytest.param(*['cephfs-provisioner', 'delete_resources'], marks=[pytest.mark.polarion_id("OCS-1242")]),
+            pytest.param(*['rook-ceph-operator', 'create_resources'], marks=[pytest.mark.polarion_id("OCS-2016")]),
+            pytest.param(*['rook-ceph-operator', 'delete_resources'], marks=[pytest.mark.polarion_id("OCS-2017")]),
         ]
     )
     def test_pv_provisioning_under_degraded_state(
-        self, nodes, pvc_factory, pod_factory, interface, operation
+        self, nodes, pvc_factory, pod_factory, resource, operation
     ):
         """
         Test PV provisioning under degraded state
@@ -113,67 +128,73 @@ class TestNodesRestart(ManageTest):
             # Create resources that their deletion will be tested later
             self.sanity_helpers.create_resources(pvc_factory, pod_factory)
 
-        provisioner_pods = None
-        # Get the provisioner pod according to the interface
-        if interface == 'rbd':
-            provisioner_pods = pod.get_rbdfsplugin_provisioner_pods()
-        elif interface == 'cephfs':
-            provisioner_pods = pod.get_cephfsplugin_provisioner_pods()
-        provisioner_pod = provisioner_pods[0]
+        resource_pods = None
+        # Get the resource pod according to the resource type
+        if resource == 'rook-ceph-operator':
+            resource_pods = pod.get_operator_pods()[0]
+        elif resource == 'rbd-provisioner':
+            resource_pods = pod.get_rbdfsplugin_provisioner_pods()
+        elif resource == 'cephfs-provisioner':
+            resource_pods = pod.get_cephfsplugin_provisioner_pods()
+        resource_pod = resource_pods[0]
+
         # Workaround for BZ 1778488 - https://github.com/red-hat-storage/ocs-ci/issues/1222
-        provisioner_node = pod.get_pod_node(provisioner_pod)
-        rook_operator_pod = pod.get_operator_pods()[0]
-        operator_node = pod.get_pod_node(rook_operator_pod)
-        if operator_node.get().get('metadata').get('name') == provisioner_node.get().get('metadata').get('name'):
-            provisioner_pod = provisioner_pods[1]
+        if not resource == 'rook-ceph-operator':
+            resource_node = pod.get_pod_node(resource_pod)
+            rook_operator_pod = pod.get_operator_pods()[0]
+            operator_node = pod.get_pod_node(rook_operator_pod)
+            if operator_node.get().get('metadata').get(
+                'name'
+            ) == resource_node.get().get('metadata').get('name'):
+                resource_pod = resource_pods[1]
         # End of workaround for BZ 1778488
 
-        provisioner_pod_name = provisioner_pod.name
+        resource_pod_name = resource_pod.name
         logger.info(
-            f"{interface} provisioner pod found: {provisioner_pod_name}"
+            f"{resource} pod found: {resource_pod_name}"
         )
 
         # Get the node name that has the provisioner pod running on
-        provisioner_node = pod.get_pod_node(provisioner_pod)
-        provisioner_node_name = provisioner_node.get().get('metadata').get('name')
+        resource_node = pod.get_pod_node(resource_pod)
+        resource_node_name = resource_node.get().get('metadata').get('name')
         logger.info(
-            f"{interface} provisioner pod is running on node {provisioner_node_name}"
+            f"{resource} pod is running on node {resource_node_name}"
         )
 
         # Stopping the nodes
-        nodes.stop_nodes(nodes=[provisioner_node])
+        nodes.stop_nodes(nodes=[resource_node])
 
         # Wait for the provisioner pod to get to running status
         selector = constants.CSI_RBDPLUGIN_PROVISIONER_LABEL if (
-            interface == 'rbd'
+            resource == 'rbd'
         ) else constants.CSI_CEPHFSPLUGIN_PROVISIONER_LABEL
 
         # Wait for the provisioner pod to reach Terminating status
         logger.info(
-            f"Waiting for pod {provisioner_pod_name} to reach status Terminating"
+            f"Waiting for pod {resource_node_name} to reach status Terminating"
         )
-        assert provisioner_pod.ocp.wait_for_resource(
-            timeout=600, resource_name=provisioner_pod.name,
+        assert resource_pod.ocp.wait_for_resource(
+            timeout=600, resource_name=resource_pod.name,
             condition=constants.STATUS_TERMINATING
-        ), f"{interface} provisioner pod failed to reach status Terminating"
+        ), f"{resource} pod failed to reach status Terminating"
         logger.info(
-            f"Pod {provisioner_pod_name} has reached status Terminating"
+            f"Pod {resource_node_name} has reached status Terminating"
         )
 
         # Wait for the provisioner pod to be started and reach running status
         logger.info(
-            f"Waiting for pod {provisioner_pod_name} to reach status Running"
+            f"Waiting for pod {resource_node_name} to reach status Running"
         )
         logger.info(
-            f"Pod {provisioner_pod_name} has reached status Running"
+            f"Pod {resource_node_name} has reached status Running"
         )
 
         # After this change https://github.com/rook/rook/pull/3642/, there are
         # 2 provisioners for each interface
-        assert provisioner_pod.ocp.wait_for_resource(
+        assert resource_pod.ocp.wait_for_resource(
             timeout=600, condition=constants.STATUS_RUNNING, selector=selector,
             resource_count=2
-        ), f"{interface} provisioner pod failed to reach status Running"
+        ), f"{resource} pod failed to reach status Running"
 
         assert wait_for_ct_pod_recovery(), "Ceph tools pod failed to come up on another node"
 
@@ -185,7 +206,7 @@ class TestNodesRestart(ManageTest):
             self.sanity_helpers.delete_resources()
 
         # Starting the nodes
-        nodes.start_nodes(nodes=[provisioner_node])
+        nodes.start_nodes(nodes=[resource_node])
 
         # Checking cluster and Ceph health
         self.sanity_helpers.health_check()
